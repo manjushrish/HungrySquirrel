59b6faa8d990650674dfa9701c50fb310fd0062e
Manju <manjushri1611@gmail.com>
2015-09-18 11:53:51 -0700

    Create PeaNuts.java

---
+++ PeaNuts.java
@@ -0,0 +1,15 @@
+public class PeaNuts extends Nuts {
+
+    public static final int nutritionPoints = 10;
+
+    PeaNuts() {
+        Sym = 'P';
+    }
+
+    @Override
+    public void create() {
+
+        super.create();
+
+    }
+}
