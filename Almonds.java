78a8d1b3f5629472cab05bf9b43781465b220844
Manju <manjushri1611@gmail.com>
2015-09-18 11:48:21 -0700

    Create Almonds.java

---
+++ Almonds.java
@@ -0,0 +1,14 @@
+public class Almonds extends Nuts {
+
+    public static final int nutritionPoints = 5;
+    Almonds() {
+        Sym = 'A';
+
+    }
+    @Override
+    public void create() {
+
+        super.create();
+
+    }
+}
